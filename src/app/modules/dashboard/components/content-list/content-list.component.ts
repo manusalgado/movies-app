c8820c4035a489862fda043a63b024c2576b65ef
Manuel Salgado <manuelsalgado@Manuels-MacBook-Pro.local>
2021-07-05 23:33:50 -0500

    Added movies list
    changes

--- src/app/modules/dashboard/components/content-list/content-list.component.ts
+++ src/app/modules/dashboard/components/content-list/content-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MoviePayload } from '../../entities/dashboard';
 
 @Component({
   selector: 'app-content-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-list.component.scss'],
 })
 export class ContentListComponent implements OnInit {
+
+  @Input() movies: MoviePayload[];
 
   constructor() { }
 
